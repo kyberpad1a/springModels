e34377f548f73a13aab0af94b348c11bc13d380d
kyberpad1a <isip_n.s.ignatov@mpt.ru>
2022-11-24 09:42:13 +0300

    Практическая 3

--- src/main/java/com/example/springmodels/controllers/employeeController.java
+++ src/main/java/com/example/springmodels/controllers/employeeController.java
@@ -7,11 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class employeeController {
@@ -57,5 +60,55 @@ public class employeeController {
 //        List<Post> result = postRepository.findLikeTitle(title);
         model.addAttribute("result", result);
         return "employee-filter";
+    }
+
+    @GetMapping("/employee/{ID_Employee}")
+    public String employeeDetails(@PathVariable(value = "ID_Employee") long ID_Employee, Model model)
+    {
+        Optional<modelEmployee> employee = employeeRepository.findById(ID_Employee);
+        ArrayList<modelEmployee> res = new ArrayList<>();
+        employee.ifPresent(res::add);
+        model.addAttribute("employee",res);
+        if(!employeeRepository.existsById(ID_Employee))
+        {
+            return "redirect:/employee";
+        }
+        return "employee-details";
+    }
+    @GetMapping("/employee/{ID_Employee}/edit")
+    public String employeeEdit(@PathVariable("ID_Employee") long ID_Employee, Model model)
+    {
+        if(!employeeRepository.existsById(ID_Employee)){
+            return "redirect:/";
+        }
+        Optional<modelEmployee> post = employeeRepository.findById(ID_Employee);
+        ArrayList<modelEmployee> res = new ArrayList<>();
+        post.ifPresent(res::add);
+        model.addAttribute("employee",res);
+        return "employee-edit";
+    }
+
+    @PostMapping("/employee/{ID_Employee}/edit")
+    public String employeeUpdate(@PathVariable("ID_Employee") long ID_Employee,
+                                 @RequestParam String employeeInitials,
+                                 @RequestParam double employeeWeight,
+                                 @RequestParam int employeeAge,
+                                 @RequestParam Date employeeDateOfBirth,
+                                 @RequestParam(defaultValue = "false") boolean employeeStatus, Model model)
+    {
+        modelEmployee employee = employeeRepository.findById(ID_Employee).orElseThrow();
+        employee.setEmployeeInitials(employeeInitials);
+        employee.setEmployeeWeight(employeeWeight);
+        employee.setEmployeeAge(employeeAge);
+        employee.setEmployeeDateOfBirth(employeeDateOfBirth);
+        employee.setEmployeeStatus(employeeStatus);
+        employeeRepository.save(employee);
+        return "redirect:/";
+    }
+    @PostMapping("employee/{ID_Employee}/remove")
+    public String employeeDelete(@PathVariable("ID_Employee") long ID_Employee, Model model){
+        modelEmployee employee = employeeRepository.findById(ID_Employee).orElseThrow();
+        employeeRepository.delete(employee);
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/springmodels/controllers/goodController.java
+++ src/main/java/com/example/springmodels/controllers/goodController.java
@@ -7,11 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class goodController {
@@ -57,5 +60,55 @@ public class goodController {
 //        List<Post> result = postRepository.findLikeTitle(title);
         model.addAttribute("result", result);
         return "good-filter";
+    }
+
+    @GetMapping("/good/{ID_Good}")
+    public String goodDetails(@PathVariable(value = "ID_Good") long ID_Good, Model model)
+    {
+        Optional<modelGood> good = goodRepository.findById(ID_Good);
+        ArrayList<modelGood> res = new ArrayList<>();
+        good.ifPresent(res::add);
+        model.addAttribute("good",res);
+        if(!goodRepository.existsById(ID_Good))
+        {
+            return "redirect:/good";
+        }
+        return "good-details";
+    }
+    @GetMapping("/good/{ID_Good}/edit")
+    public String goodEdit(@PathVariable("ID_Good") long ID_Good, Model model)
+    {
+        if(!goodRepository.existsById(ID_Good)){
+            return "redirect:/good";
+        }
+        Optional<modelGood> good = goodRepository.findById(ID_Good);
+        ArrayList<modelGood> res = new ArrayList<>();
+        good.ifPresent(res::add);
+        model.addAttribute("good",res);
+        return "good-edit";
+    }
+
+    @PostMapping("/good/{ID_Good}/edit")
+    public String goodUpdate(@PathVariable("ID_Good") long ID_Good,
+                             @RequestParam String goodName,
+                             @RequestParam double goodPrice,
+                             @RequestParam int goodQuantity,
+                             @RequestParam Date goodSupplyDate,
+                             @RequestParam(defaultValue = "false") boolean goodDefect, Model model)
+    {
+        modelGood good = goodRepository.findById(ID_Good).orElseThrow();
+        good.setGoodName(goodName);
+        good.setGoodPrice(goodPrice);
+        good.setGoodQuantity(goodQuantity);
+        good.setGoodSupplyDate(goodSupplyDate);
+        good.setGoodDefect(goodDefect);
+        goodRepository.save(good);
+        return "redirect:/good";
+    }
+    @PostMapping("good/{ID_Good}/remove")
+    public String goodDelete(@PathVariable("ID_Good") long ID_Good, Model model){
+        modelGood good = goodRepository.findById(ID_Good).orElseThrow();
+        goodRepository.delete(good);
+        return "redirect:/good";
     }
 }
